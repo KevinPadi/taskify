38e902ad1e140808df2a89a795b37c0260f9315d
Kevin <kevpadilla01@gmail.com>
2025-01-13 16:54:46 -0300

    fix (ProjectsLink): added Link component to go to kanban board view

--- src/components/ProjectsLink.tsx
+++ src/components/ProjectsLink.tsx
@@ -4,16 +4,19 @@ import { Button } from "./ui/button"
 
 const ProjectsLink: React.FC<{ item: { name: string; background: string; _id: string } }> = ({ item }) => {
   return (
-    <Link to={`/kanban/${item.name}`}>
+    <span>
       <article className="flex items-center justify-between group">
       <Button variant={'link'} asChild className="text-neutral-900 dark:text-neutral-300 font-normal">
-        <a href={'#'}> 
-          <span>{item.name}</span>
-        </a>
+        <Link 
+          to={`/kanban/${item.name}/${item._id}`}
+          state={{ imageUrl: item.background }}
+        > 
+          {item.name}
+        </Link>
       </Button>
       <CardDropdownMenu name={item.name} imageUrl={item.background} _id={item._id} onInteraction={(e) => e.stopPropagation()} />
       </article>
-    </Link>
+    </span>
   )
 }
 
